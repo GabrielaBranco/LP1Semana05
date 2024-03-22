6a999691600b50c8cb0f9744818204fa6af284d9
Gabriela Branco <lindosogb@gmail.com>
2024-03-22 14:54:51 +0000

    adds Random Dice

---
+++ RanDice/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+            int[] sequence = new int[n];
+            int result = 0;
+            
+            Random dice = new Random(s);
+            for(int i = 0; i<n ; i++)
+            {
+                int diceInt = dice.Next(1, 6);
+
+                sequence[i] = diceInt;
+            }
+
+            foreach(int num in sequence)
+            {
+                result += num;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
